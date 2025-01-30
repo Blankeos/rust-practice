8d713970c105d06c6832bf5cfef22819829b16d4
Blankeos <carloantonioct@gmail.com>
2025-01-31 03:41:29 +0800

    feat(rustlings): 006 move semantics (borrow/ownership) done.

--- rustlings/exercises/06_move_semantics/move_semantics4.rs
+++ rustlings/exercises/06_move_semantics/move_semantics4.rs
@@ -10,9 +10,16 @@ mod tests {
     fn move_semantics4() {
         let mut x = Vec::new();
         let y = &mut x;
-        let z = &mut x;
         y.push(42);
+
+        let z = &mut x;
         z.push(13);
         assert_eq!(x, [42, 13]);
+
+        // How this was solved:
+        // let y = &mut x;
+        // let z = &mut x;
+        // Is not possible because there can only be 1 'mutable borrows' of something at 1 time.
+        // Solution: Mutably borrow, then use once. Then you can mutably borrow again.
     }
 }
