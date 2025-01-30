3173772d544af3dfff40912b89e4dc25b96443dc
Blankeos <carloantonioct@gmail.com>
2025-01-31 03:42:21 +0800

    chore(rustlings): quiz1 done.

--- rustlings/exercises/quizzes/quiz1.rs
+++ rustlings/exercises/quizzes/quiz1.rs
@@ -10,7 +10,21 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    // let price = if quantity > 40 {
+    //     quantity
+    // } else {
+    //     quantity * 2
+    // };
+
+    // price
+
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
