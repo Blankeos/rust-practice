116df40a338fc853ecfdd155db44087763e6d119
Blankeos <carloantonioct@gmail.com>
2025-01-31 03:41:03 +0800

    feat(rustlings): 004 primitive types done.

--- rustlings/exercises/04_primitive_types/primitive_types5.rs
+++ rustlings/exercises/04_primitive_types/primitive_types5.rs
@@ -3,6 +3,17 @@ fn main() {
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
+    let (name, age) = cat;
 
     println!("{name} is {age} years old");
+
+    // Notes by Carlo: I can also use it to swap. This is very Javascripty, but it works.
+    let a = "Carlo";
+    let b = "Awesome";
+
+    println!("a: {a} | b: {b}");
+
+    let (b, a) = (a, b);
+
+    println!("a: {a} | b: {b}");
 }
