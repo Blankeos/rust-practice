a744f27ad6faa1e0af73f2545fad5b91e549ecd1
Blankeos <carloantonioct@gmail.com>
2025-03-01 00:01:12 +0800

    feat(rustlings): enums and strings done.

--- rustlings/exercises/09_strings/strings1.rs
+++ rustlings/exercises/09_strings/strings1.rs
@@ -1,9 +1,19 @@
 // TODO: Fix the compiler error without changing the function signature.
 fn current_favorite_color() -> String {
-    "blue"
+    "blue".to_string()
 }
 
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {answer}");
 }
+
+// So what I learned:
+// String vs &str
+// - &str is "" - it's a 'slice slice' in a 'static' lifetime.
+// - String can be created in multiple ways:
+//   - String::from().
+//   - "".to_string()
+//   - "".to_owned()
+//   - "".into()
+//   - format!()
